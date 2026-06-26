26b8c57aaa496a4b3aeb1fb760ab6ed11f1c46fa
honey <honey1207meow@gmail.com>
2026-06-26 13:15:34 +0200

    Bonuses are calculated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,22 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+    int initialBalance = 100;
+    int refillAmount = 1100;
 
-        }
+    int bonus;
+      if (refillAmount > 1000) {
+        bonus = refillAmount/100;
+      } else {
+          bonus = 0;
+      }
+
+    int finalBalance = initialBalance + refillAmount + bonus;
+
+        System.out.println("Начальный баланс: " + initialBalance + " руб.");
+        System.out.println("Сумма пополнения: " + refillAmount + " руб.");
+        System.out.println("Бонусы: " + bonus + " руб.");
+        System.out.println("Итоговый баланс: " + finalBalance + " руб.");
+
     }
+}
